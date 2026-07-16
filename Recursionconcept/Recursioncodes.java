60dfafacc5bddbc51dbe8acaaacb48d89dd482aa
Anjali <shishodiaanjali597@gmail.com>
2026-07-16 19:52:43 +0530

    Added recursion codes

---
+++ Recursionconcept/Recursioncodes.java
@@ -0,0 +1,55 @@
+package Recursionconcept;
+//Print Name n times using Recursion
+    /*public class Recursioncodes{
+        static void f(int i, int n) {
+        if (i > n) {
+            return;
+        }
+
+        System.out.println("Raj");
+        f(i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        f(1, n);
+    }
+    }*/
+
+// Print number from 1 to n using Recursion
+/*public class Recursioncodes{
+    static void f(int i, int n) {
+        if (i > n) {
+            return;
+        }
+
+        System.out.println(i);
+        f(i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        f(1, n);
+    }
+}*/
+
+// Print number from n to 1 usng Recursion 
+/*public class Recursioncodes {
+
+    static void f(int i) {
+        if (i < 1) {
+            return;
+        }
+
+        System.out.println(i);
+        f(i - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        f(n);
+    }
+}*/
+
+
+
